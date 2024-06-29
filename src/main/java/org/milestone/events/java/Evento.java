cddd067736ae734fa021625ac0c8b06ad677fc46
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-29 21:15:31 +0200

    feat: metodi prenota, cancella, toString

--- src/main/java/org/milestone/events/java/Evento.java
+++ src/main/java/org/milestone/events/java/Evento.java
@@ -1,6 +1,7 @@
 package org.milestone.events.java;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
     
@@ -45,4 +46,50 @@ public class Evento {
 		return bookedSeats;
 	}
 	
+	/**
+	 * Metodo che aggiunge un posto tra quelli prenotati, a condizione che:
+	 * I posti non siano finiti (con re),
+	 * La data non sia passata (con stampa di messaggio dedicato)
+	 */
+	
+	/**
+	 * Metodo per l'aggiunta di un posto alla prenotazione,
+	 * @return String risultato del tentativo di prenotazione
+	 */
+	public String book() {
+		if (this.bookedSeats == this.totalSeats) {
+			return "Attenzione, non ci sono più posti disponibili";
+		} else if (LocalDate.now().isAfter(this.date)) {
+			return "Attenzione, la data dell'evento è già passata";
+		} else {
+			this.bookedSeats++;
+			return "Posto prenotato con successo";
+		}
+	}
+	
+	/**
+	 * Metodo per la cancellazione di una prenotazione,
+	 * @return String risultato del tentativo di cancellazione
+	 */
+	public String cancel() {
+		if (this.bookedSeats == 0) {
+			return "Attenzione, non ci sono prenotazioni da disdire";
+		} else if (LocalDate.now().isAfter(this.date)) {
+			return "Attenzione, la data dell'evento è già passata";
+		} else {
+			this.bookedSeats++;
+			return "Prenotazione cancellata con successo";
+		}
+	}
+	
+	/**
+	 * Oveddire del metodo toString,
+	 * Restituisce la data formattata tramite un oggetto formatter, più il titolo
+	 */
+	@Override
+	public String toString() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String formattedDate = this.date.format(formatter);		
+		return String.format("%s - %s", formattedDate, this.title);
+	}
 }
