06c44f3b05819e71619b3d0502ce2b640b8d241d
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-29 20:30:35 +0200

    feat: uso di LocalDate

--- src/main/java/org/milestone/events/java/Evento.java
+++ src/main/java/org/milestone/events/java/Evento.java
@@ -1,22 +1,32 @@
 package org.milestone.events.java;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Evento {
 
 	private String title;
-	private Date date;
+	private LocalDate date;
 	private int seats;
 	private int bookedSeats;
 	
-	public Evento(String title, Date date, int seats) {
+	public Evento(String title, LocalDate date, int seats) {
 		this.title = title;
 		this.date = date;
-		// se la data attuale è maggiore o uguale alla data dell'evento,
-		if (new Date().compareTo(this.date) >= 0) System.out.println("Attenzione, l'evento è stato creato come già passato");
+		// se la data attuale è superiore alla data dell'evento, stampa avviso all'utente
+		if (LocalDate.now().isAfter(this.date)) System.out.println("Attenzione, l'evento è stato creato come già passato");
 		this.seats = seats;
+		// se il numero di posti istanziato è minore di 1, stampa avviso all'utente
 		if (seats <= 0) System.out.println("Attenzione, l'evento è stato creato senza posti disponibili");
 		this.bookedSeats = 0;
+	}
+	
+	// getters e setters
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
 	}
 	
 }
