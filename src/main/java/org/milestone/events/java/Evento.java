2a161a67a9ef388bd31c43fa90481d8f4b703f6b
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-29 20:36:10 +0200

    feat: getters e setters di Evento

--- src/main/java/org/milestone/events/java/Evento.java
+++ src/main/java/org/milestone/events/java/Evento.java
@@ -3,10 +3,10 @@ package org.milestone.events.java;
 import java.time.LocalDate;
 
 public class Evento {
-
+    
 	private String title;
 	private LocalDate date;
-	private int seats;
+	private final int totalSeats;
 	private int bookedSeats;
 	
 	public Evento(String title, LocalDate date, int seats) {
@@ -14,7 +14,7 @@ public class Evento {
 		this.date = date;
 		// se la data attuale è superiore alla data dell'evento, stampa avviso all'utente
 		if (LocalDate.now().isAfter(this.date)) System.out.println("Attenzione, l'evento è stato creato come già passato");
-		this.seats = seats;
+		this.totalSeats = seats;
 		// se il numero di posti istanziato è minore di 1, stampa avviso all'utente
 		if (seats <= 0) System.out.println("Attenzione, l'evento è stato creato senza posti disponibili");
 		this.bookedSeats = 0;
@@ -27,6 +27,22 @@ public class Evento {
 	
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	
+	public LocalDate getDate() {
+		return this.date;
+	}
+	
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public int getSeats() {
+		return this.totalSeats;
+	}
+	
+	public int getbooked() {
+		return bookedSeats;
 	}
 	
 }
