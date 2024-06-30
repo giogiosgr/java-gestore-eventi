d660661cbecb69661bed3f728951d18f8a53148c
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-30 11:52:44 +0200

    feat: test di prenotazioni e cancellazioni

--- src/main/java/org/milestone/events/java/Main.java
+++ src/main/java/org/milestone/events/java/Main.java
@@ -9,11 +9,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// dichiarazione variabili e oggetti vari
+		// dichiarazione variabili e oggetti per la successiva istanza e utilizzo della classe Evento
 		String newTitle;
         int newSeats = 0;	
 		String newDate;
-		boolean error;
+		int modifiedSeats = 0;
 		Scanner sc = new Scanner(System.in);
 		/* creazione oggetto formatter per poter assegnare in seguito un valore al local date,
 		 * nel formato desiderato.
@@ -22,10 +22,11 @@ public class Main {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		LocalDate parseddDate = LocalDate.now();
 		/* serie di input per la creazione del nuovo evento,
-		 * con cicli e gestione delle eccezioni per l'inserimento corretto della data e del numero di posti
+		 * con cicli per gestione delle eccezioni per l'inserimento corretto della data e del numero di posti
 		 */
 		System.out.println("Inserire il nome dell'evento: ");
 		newTitle = sc.nextLine();
+		boolean error;
 		do {
 		    error = false;		
 		    System.out.println("\nInserire data del nuovo evento nel formato GG/MM/AAAA: ");  
@@ -45,23 +46,57 @@ public class Main {
 			try {
 				newSeats = sc.nextInt();
 			} catch (Exception e) {			
-				System.out.println("Numero non valido!");
-				sc.nextLine();
+				System.out.println("Formato del numero non valido!");
 				error = true;
 			}
+			sc.nextLine();
 		} while (error);
-		sc.close();
 		// istanza di un nuovo oggetto di classe Evento
 		Evento newEvent = new Evento(newTitle, parseddDate, newSeats);
-		System.out.println("\nCreato il seguente evento: " + newEvent.toString());
-        // serie di input per prenotare e disdire posti dell'evento creato
-		
-        System.out.println("\n" + newEvent.book(5));
-        
-        System.out.println("\n" + newEvent.cancel(5));
-		 
-		
-		
-	}
-
+		System.out.println("\nCreato il seguente evento: " + newEvent.toString()); // stampa informazioni evento
+        /* serie di richieste di input per prenotazione e cancellazione, 
+         * con serie di cicli per controllo delle scelte dell'utente e validità dell'input
+         */
+		String answer;			
+		do {
+			System.out.println("\nPrenotare nuovi posti? (s per confermare, altro tasto per continuare): ");
+			answer = sc.nextLine().toLowerCase();
+	   	    if (answer.equals("s")) {
+	   	    	do {
+		            error = false;
+		            System.out.println("\nInserire il numero di posti da prenotare: ");
+		     	    try {
+				        modifiedSeats = sc.nextInt();
+			        } catch (Exception e) {			
+				        System.out.println("Formato del numero non valido!");
+				        error = true;
+			        }
+	   	    	} while (error);
+			    sc.nextLine();
+			    System.out.println("\n" + newEvent.book(modifiedSeats)); // stampa del risultato della prenotazione
+		    }	    
+		} while(answer.equals("s"));
+		do {
+			System.out.println("\nCancellare posti prenotati? (s per confermare, altro tasto per continuare): ");
+			answer = sc.nextLine().toLowerCase();
+	   	    if (answer.equals("s")) {
+	   	    	do {
+		            error = false;
+		            System.out.println("\nInserire il numero di posti da cancellare dalla prenotazione: ");
+		     	    try {
+				        modifiedSeats = sc.nextInt();
+			        } catch (Exception e) {			
+				        System.out.println("Formato del numero non valido!");
+				        error = true;
+			        }
+	   	    	} while (error);
+			    sc.nextLine();
+			    System.out.println("\n" + newEvent.cancel(modifiedSeats)); // stampa del risultato della cancellazione
+		    }	    
+		} while(answer.equals("s"));
+        sc.close();    		
+        // test su altri metodi
+        System.out.println("\n" + newEvent.book());
+        System.out.println("\n" + newEvent.cancel());
+	}	
 }
