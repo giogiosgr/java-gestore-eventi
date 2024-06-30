472d101f7d489e069e26fb86f1f59a75d8afd373
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-06-30 16:18:43 +0200

    feat: metodo getFormattedTime (e nuovo toString)

--- src/main/java/org/milestone/events/java/Concerto.java
+++ src/main/java/org/milestone/events/java/Concerto.java
@@ -2,12 +2,21 @@ package org.milestone.events.java;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-public class Concerto extends Evento{
-	
+public class Concerto extends Evento {
+	// variabili di istanza
 	private LocalTime time;
 	private double price;
 	
+	/**
+	 * Costruttore
+	 * @param title
+	 * @param date
+	 * @param totalSeats
+	 * @param time
+	 * @param price
+	 */
 	public Concerto(String title, LocalDate date, int totalSeats, LocalTime time, double price) {
 		super(title, date, totalSeats);
 		this.time = time;
@@ -28,6 +37,18 @@ public class Concerto extends Evento{
 	
 	public void setPrice(double price) {
 		this.price = price;
+	}
+	
+	public String getFormattedTime() {
+		return this.time.format(DateTimeFormatter.ofPattern("HH:mm"));	
+	}
+	
+	@Override
+	public String toString() {
+		String formattedPrice = String.format("%.2f€", this.price);
+	 
+		
+		return formattedPrice;
 	}
 
 }
