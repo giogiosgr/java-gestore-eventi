86911c9787bc735f9496f9cb0b50859092182195
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-07-02 00:34:27 +0200

    feat: metodi get lista ordinata e svuota lista

--- src/main/java/org/milestone/events/java/ProgrammaEventi.java
+++ src/main/java/org/milestone/events/java/ProgrammaEventi.java
@@ -2,6 +2,7 @@ package org.milestone.events.java;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ProgrammaEventi {
 	// variabili di istanza
@@ -52,10 +53,51 @@ public class ProgrammaEventi {
 	    }
 		return newList;
 	}
-
+	
+	/**
+	 * Metodo che restituisce una stringa con il titolo del programma e tutti gli eventi ordinati per data,
+	 * Per l'ordinamento degli eventi, per data, è utilizzato l'algoritmo di Bubble Sort
+	 * @return String
+	 */
 	public String getOrderedEvents() {
-        return "ciao";
+		String fullProgram = ("Lista eventi di " + this.title);
+		ArrayList<Evento> orderedList = new ArrayList<>(this.eventList);
+		boolean swapped;
+		do {
+			swapped = false;
+			for (int i = 0; i < orderedList.size()-1; i++) {
+				if (orderedList.get(i).getDate().isAfter(orderedList.get(i+1).getDate())) {
+					Collections.swap(orderedList, i, i+1);
+					swapped = true;
+				}
+			}
+		} while (swapped);
+		for (Evento x : orderedList) {
+			fullProgram += String.format("\n%s - %s", x.getFormattedDate(), x.getTitle());
+		}
+        return fullProgram;
    	}
-             
-
+	
+	/**
+	 * Metodo che restituisce il numero di eventi nella lista
+	 * @return int
+	 */
+	public int getEventsCount() {
+		int count = 0;
+		int i = 0;
+		while (eventList.get(i) != null) {
+			count++;	
+		    i++;
+		}
+		return count;
+	}
+	
+	/**
+	 * Metodo che svuota la lista
+	 */
+	public void clearEventList() {
+		while (this.eventList.size() > 0) {
+			eventList.remove(0);	
+		}
+	}
 }
