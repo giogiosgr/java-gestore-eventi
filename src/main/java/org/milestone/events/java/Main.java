040d38a2fc51d9208b4e2c4b9dcdc2f1567c659f
giogiosgr <giovannisgr88@gmail.com>
2024-07-01 16:06:24 +0200

    feat: metodi dedicati all'input

--- src/main/java/org/milestone/events/java/Main.java
+++ src/main/java/org/milestone/events/java/Main.java
@@ -13,16 +13,13 @@ public class Main {
 		// dichiarazione variabili e oggetti per la successiva istanza e utilizzo della classe Evento
 		String newTitle;
 		int newSeats = 0;
-		String newDate;
 		int modifiedSeats = 0;
 		Scanner sc = new Scanner(System.in);
-		/* creazione oggetto formatter per poter assegnare in seguito un valore al local date,
-		 * nel formato desiderato.
-		 * quindi successiva istanza di un oggetto LocalDate, inizialmente con la data corrente
+		/* creazione oggetto formatter per poter assegnare un valore al local date nel formato personalizzato,
+		 * quindi successiva dichiarazione di un oggetto LocalDate
 		 */
 		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		LocalDate parsedDate = LocalDate.now();
-		
+		LocalDate parsedDate;
 		
 		// ----- WIP PROVE PROGRAMMAEVENTI -----
 		
@@ -36,67 +33,26 @@ public class Main {
 		
 		System.out.println(newProgram.getEventsByDate(LocalDate.parse("2025-12-12")));
 		
-		
-		
-		
-		
 		// -------------------------------------
 		
-		
-		
-		/* serie di input per la creazione del nuovo evento,
-		 * con cicli per gestione delle eccezioni per l'inserimento corretto della data e del numero di posti
-		 */
-		System.out.println("Inserire il nome dell'evento: ");
+		// serie di input, con richiamo delle funzioni dedicate per input della data e dei numeri interi
+		System.out.println("Inserire il titolo dell'evento");
 		newTitle = sc.nextLine();
-		boolean error;
-		do {
-		    error = false;		
-		    System.out.println("\nInserire data del nuovo evento nel formato GG/MM/AAAA: ");  
-		    try {    	
-		    	newDate = sc.nextLine();
-		    	// assegnazione della data dalla stringa di input, dal formato determinato con formatter
-			    parsedDate = LocalDate.parse(newDate, dateFormatter);
-            } catch (DateTimeParseException e) {
-                System.out.println("Formato della data non valido!");
-                error = true;
-            }
-		}  
-		while (error);  	
-		do {
-			error = false;
-		    System.out.println("\nInserire il numero di posti disponibili: ");
-			try {
-				newSeats = sc.nextInt();
-			} catch (Exception e) {
-				System.out.println("Formato del numero non valido!");
-				error = true;
-			}
-			sc.nextLine();
-		} while (error);
+		parsedDate = checkDateInput("Inserire la data nel formato gg/mm/aaaa", sc, dateFormatter);
+		newSeats = checkIntInput("Inserire il numero di posti disponibili", sc);
 		// istanza di un nuovo oggetto di classe Evento
 		Evento newEvent = new Evento(newTitle, parsedDate, newSeats);
 		System.out.println("\nCreato il seguente evento: " + newEvent); // stampa informazioni evento
-        /* richieste di input per prenotazione o cancellazione, 
-         * con serie di cicli, con controllo delle scelte dell'utente e validità dell'input
-         */
+        // ciclo per l'input delle prenotazioni, con richiamo alla funzione dedicata per input di interi
 		String answer;			
 		do {
-			System.out.println("\nPrenotare o cancellare posti? (p per prenotare, c per cancellare, altro tasto per continuare): ");
+			System.out.println("\nPrenotare o cancellare posti? (p per prenotare, c per cancellare, altro tasto per continuare)");
 			answer = sc.nextLine().toLowerCase();
 	   	    if (answer.equals("p") || answer.equals("c")) {
-	   	    	do {
-		            error = false;
-		            if (answer.equals("p")) System.out.println("\nInserire il numero di posti da prenotare: ");
-		            if (answer.equals("c")) System.out.println("\nInserire il numero di posti da cancellare dalla prenotazione: ");
-		     	    try {
-				        modifiedSeats = sc.nextInt();
-			        } catch (Exception e) {
-				        System.out.println("Formato del numero non valido!");
-				        error = true;
-			        }
-				    sc.nextLine();
-	   	    	} while (error);
+				String message = "";
+	   	    	if (answer.equals("p")) message = "Inserire il numero di posti da prenotare";
+				if (answer.equals("c")) message = "Inserire il numero di prenotazioni da cancellare";
+                modifiedSeats = checkIntInput(message, sc);
 			    // stampa del risultato della prenotazione
 			    if (answer.equals("p")) System.out.println("\n" + newEvent.book(modifiedSeats)); 
 			    // stampa del risultato della cancellazione
@@ -113,4 +69,54 @@ public class Main {
         Concerto newConcert = new Concerto("Concertone", parsedDate, 100, parsedTime, 39.90);
         System.out.println("\nRiepilogo concerto: \n" + newConcert.toString());    
 	}	
+
+	/**
+	 * Metodo per l'acquisizione di un oggetto LocalDate, passando la stringa della richiesta,
+	 * Poi un oggetto scanner, quindi un oggetto formatter per l'acquisizione della data nella forma richiesta,
+	 * Comprende il controllo del corretto inserimento dell'input tramite costrutto try/catch
+	 * @param question
+	 * @param input
+	 * @param customFormatter
+	 * @return LocalDate
+	 */
+	public static LocalDate checkDateInput(String question, Scanner input, DateTimeFormatter customFormatter) {
+		LocalDate inputDate = LocalDate.now();
+		boolean error;
+		do {
+			error = false;		
+			System.out.println("\n" + question); 
+			try {  
+				inputDate = LocalDate.parse(input.nextLine(), customFormatter);
+			} catch (DateTimeParseException e) {
+				System.out.println("Formato della data non valido!");       
+				error = true;
+			}
+		}  
+		while (error);  	
+		return inputDate;
+	}
+    
+	/**
+	 * Metodo per l'acquisizione di un intero, passando la stringa della richiesta e un oggetto scanner,
+	 * Comprende il controllo del corretto inserimento dell'input tramite costrutto try/catch
+	 * @param question
+	 * @param input
+	 * @return int
+	 */
+    public static int checkIntInput(String question, Scanner input) {
+		int inputNumber = 0;
+		boolean error;
+		do {
+			error = false;
+			System.out.println("\n" + question);
+			try {
+				inputNumber = input.nextInt();
+			} catch (Exception e) {
+				System.out.println("Formato del numero non valido!");
+				error = true;    
+			}
+			input.nextLine();	    
+			} while (error);
+		return inputNumber;
+	}
 }
