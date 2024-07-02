06a80c7c357949575a0af0242174b6360f738817
giogiosgr <giovannisgr88@gmail.com>
2024-07-02 09:35:35 +0200

    fix: spacing dell'output

--- src/main/java/org/milestone/events/java/Main.java
+++ src/main/java/org/milestone/events/java/Main.java
@@ -49,12 +49,14 @@ public class Main {
         System.out.println("\n" + newEvent.book());
         System.out.println("\n" + newEvent.cancel());
    	    // istanza e test della classe Concerto
+		System.out.println("\nCLASSE CONCERTO\n");
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
         LocalTime parsedTime = LocalTime.parse("20:30", timeFormatter);
         Concerto newConcert = new Concerto("Concertone", newDate, 100, parsedTime, 39.90);
         System.out.println("\nRiepilogo concerto: \n" + newConcert);   
 		// istanta e test della classe ProgrammaEventi
-		ProgrammaEventi newProgram = new ProgrammaEventi("programmone");
+		System.out.println("\nCLASSE PROGRAMMAEVENTI\n");
+		ProgrammaEventi newProgram = new ProgrammaEventi("Programmone");
 		newProgram.addEvent(new Evento("evento1", LocalDate.parse("2028-12-11"), 100));
 		newProgram.addEvent(new Evento("evento2", LocalDate.parse("2025-12-12"), 100));
 		newProgram.addEvent(new Evento("evento3", LocalDate.parse("2027-01-02"), 100));
